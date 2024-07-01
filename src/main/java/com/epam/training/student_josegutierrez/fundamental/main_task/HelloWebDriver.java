90c10fde8274270c32a68fbfd6e248670b62cd00
Jose Gutierrez <jlgv1989@gmail.com>
2024-06-30 23:09:29 -0700

    Updates and Test

---
+++ src/main/java/com/epam/training/student_josegutierrez/fundamental/main_task/HelloWebDriver.java
@@ -0,0 +1,15 @@
+package com.epam.training.student_josegutierrez.fundamental.main_task;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class HelloWebDriver
+{
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.example.com/");
+        Thread.sleep(2000);
+        driver.quit();
+    }
+}
