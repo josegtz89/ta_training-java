d886ef9e86337189f913e0f6eac95e417493e0d3
Jose Gutierrez <jlgv1989@gmail.com>
2024-06-30 18:50:29 -0700

    Java Project Setup

---
+++ src/main/java/com/epam/training/student_josegutierrez/App.java
@@ -0,0 +1,13 @@
+package com.epam.training.student_josegutierrez;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
